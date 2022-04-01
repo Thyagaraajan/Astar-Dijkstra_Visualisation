fdc28ad0ae42407b3959545b39cf539f4720b2f8
Thyagaraajan <80024953+Thyagaraajan@users.noreply.github.com>
2022-04-01 22:17:19 +0530

    Add files via upload

---
+++ pet/owner.h
@@ -0,0 +1,34 @@
+class owner;
+{
+    char owner_name[20];
+    public;
+    ownwer();
+    void print()
+    pet*p;
+};
+owner::owner()
+{
+    cout<<"enter the ownwer name";
+    cin>>ownwer_name;
+    char ch;
+    cout<<"enter y you want   pet address";
+    cin>>ch;
+    if(ch=='Y'||ch=='y')
+    {
+        p=new pet;
+    }
+    else
+    {
+        p=NULL;
+        cout<<"ownwer_name;
+
+    }
+}
+void::print()
+{
+    cout<<owner_name;
+    if(p!=NULL)
+    {
+        P->print();
+    }
+}
